1849fd7820f397411d0af85a277b4f811f6736d0
Sowjanya Tirumala <sowjanyatirumala@outlook.com>
2023-08-08 00:41:22 -0500

    Spring Boot and REST api - Initial commit

---
+++ src/main/groovy/com/springframework/learning/demorestapi/DemoRestApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.springframework.learning.demorestapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DemoRestApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DemoRestApiApplication, args)
+	}
+
+}
